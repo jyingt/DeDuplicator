8110262772cd0efe6976536954dcaee61f3f059c
John Ying <yingj@bu.edu>
2014-02-06 16:13:30 -0500

    Added sample class.

---
+++ src/deduplicator/DeDuplicator.java
@@ -0,0 +1,14 @@
+package deduplicator;
+
+public class DeDuplicator
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a deduplicator... yet.");
+    }
+
+    public DeDuplicator()
+    {
+         //
+    }
+}
