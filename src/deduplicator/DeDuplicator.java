86cc6935c2748ec8793cfd512cb695e3e13c694c
zhswatson <zhswatson@gmail.com>
2014-02-15 10:41:45 -0500

    I added sth to DeDuplicator.java to see how it works

--- src/deduplicator/DeDuplicator.java
+++ src/deduplicator/DeDuplicator.java
@@ -5,6 +5,7 @@ public class DeDuplicator
     public static void main(String[] args)
     {
         System.out.println("This is a deduplicator... yet.");
+	System.out.println("Just Checking how clone works:)-Hansen");
     }
 
     public DeDuplicator()
