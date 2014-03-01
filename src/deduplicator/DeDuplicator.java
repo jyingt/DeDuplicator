c8c22728315072d212a002fb49fecf46fbe415f6
John Ying <yingj@bu.edu>
2014-03-01 12:12:26 -0500

    Command line arguments interface

--- src/deduplicator/DeDuplicator.java
+++ src/deduplicator/DeDuplicator.java
@@ -5,54 +5,69 @@ import java.util.*;
 
 public class DeDuplicator
 {
-    public static void main(String[] args) throws IOException
+    public DeDuplicator()
     {
-        System.out.println("This is a deduplicator... yet.");
-        System.out.println("Just Checking how clone works:)-Hansen");
+         //
+    }
+    
+    public static void main(String[] args) throws IOException
+    {   
+        // Test file
+        File file;
         
-        // Sample file I/O operations
-        // Example: http://www.tutorialspoint.com/java/java_filereader_class.htm
+        // Reader for test file
+        FileReader fileReader;
         
+        // Command line arguments
+        if (args.length != 2) {
+            // Leave program
+            System.out.println("usage: store -file [file]");
+            //System.out.println("usage: store -file [file] -locker [locker location]");
+            return;
+        }
+        else {
+            // Reads file name from argument
+            if (args[0].equals("-file")) {
+                file = new File(args[1]);
+                fileReader = new FileReader(file);
+            }
+            else {
+                return;
+            }
+        }
+        
+        // EXAMPLE: Read file line by line
+        // Reference: http://www.mkyong.com/java/how-to-read-file-from-java-buffReader-example/
+        System.out.println("Reading " + file.getName() + " line by line...");
+        
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        
+        String str;
+        while((str = bufferedReader.readLine()) != null)
+            System.out.println(str);
+        
+        bufferedReader.close();
+        fileReader.close();
+
+        /*
         // Create file
-        File file = new File("hello.txt");
+        File file = new File("test_file_01.txt");
         file.createNewFile();
         
         // Write to file
         FileWriter writer = new FileWriter(file);
         
-        // Write 10MB of ASCII (one char is 16 bits)
+        // Write 10MB of ASCII (each char is 2 bytes)
+        // # of iterations = 10 * 1024B / 2B = 10 * 512B = 10B * 2^9
         for (int ii = 0; ii < 10 * (2 << 9); ii++) {
             if (ii % 27 == 26)
                 writer.write("\n");
             else
-                writer.write(Character.toString((char) ('a' + ii % 26)));
+                writer.write(Character.toString((char) ('a' + ii % 27)));
         }
         
         writer.flush();
         writer.close();
-        
-        // Read from file
-        FileReader reader = new FileReader(file);
-        
-        // Read character by character
-        char[] a = new char[50];
-        reader.read(a);
-        
-        for (char c: a)
-            System.out.print(c);
-        
-        // Read line by line
-        // Example: http://www.mkyong.com/java/how-to-read-file-from-java-bufferedreader-example/
-        BufferedReader bReader = new BufferedReader(reader);
-        String str;
-        while ((str = bReader.readLine()) != null)
-            System.out.println(str);
-        
-        reader.close();
-    }
-
-    public DeDuplicator()
-    {
-         //
+        */
     }
 }
